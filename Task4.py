45b80d87584cb6f8aef431bee6b4c4edd2950348
Taisia Kashintseva <the_smartest_peach@mail.ru>
2023-02-20 21:27:36 +0700

    Task4 partially done (A lot useless information in console)

--- Task4.py
+++ Task4.py
@@ -1,49 +1,33 @@
-from github import Github
 import subprocess
+import os
 
 
-def check(commit, command):
-    commit.get_check_runs(command)
-
-
-# def binary_search(nums, command):
-#     l = 0
-#     r = len(nums)
-#
-#     while l < r:
-#         middle = l + (r - l) // 2
-#         if check(nums[middle], command):
-#             return middle
-#         elif nums[middle] < target:
-#             l = middle + 1
-#         else:
-#             r = middle
-#     return -1
+def binary_search(commits, repository, command):
+    l = 0
+    r = len(commits)
+    middle = 0
+    while l < r:
+        middle = l + (r - l) // 2
+        subprocess.run(['git', 'checkout', commits[middle]])
+        output = subprocess.run(['python', repository + command])
+        if output.returncode == 0:
+            r = middle
+        else:
+            l = middle + 1
+    return commits[middle]
 
 
 def git_bisect(repository, start, finish, command):
-    #  Получаем нужные коммиты
-    g = Github()
-    repo = g.get_repo(repository)
-    commits = []
-    for commit in repo.get_commits():
-        commits.append(commit.sha)
+    os.chdir(repository)
+    commits = subprocess.check_output(
+        ['git', 'log', start, finish, '--pretty=format:%h']).decode("utf-8").split('\n')
     start_ind = commits.index(start) + 1
     finish_ind = commits.index(finish)
-    commits = commits[finish_ind:start_ind]  # Массив необходимых коммитов
-    # answer = binary_search(commits, command)
-    # for i in repo.get_commits():
-    #     i.get_check_runs(check_name=command, )
-    cmd = ('python3', '-m', 'pycodestyle', 'main.py', '--ignore', 'W191')
-    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
-    if result.returncode == 0:
-        print('good')
-        return 1
-    else:
-        print('Oh...')
-        return 0
+    suitable_commits = commits[finish_ind:start_ind]
+    result = binary_search(suitable_commits, repository, command)
+    print("result: ", result)
 
 
-start = "e98664f26daea85836902023e05b1fb785a566b2"
-finish = "708a9ff9ed19df7a730a708ef52911616ecf3789"
-git_bisect('Smart-Peach/Testing', start, finish, 'main.py')
+start = 'f3edc5d'
+finish = '708a9ff'
+git_bisect('C:\\Users\\Home\\Desktop\\PYTHON\\Testing', start, finish, '\\main.py')
