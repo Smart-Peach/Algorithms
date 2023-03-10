6eb79749d0627b19361aa0809b041792a2a7e67d
Taisia Kashintseva <the_smartest_peach@mail.ru>
2023-03-11 01:14:47 +0700

    Task7: recursion works but TIME LIMIT EXCEEDED (T_T)

--- Task7.py
+++ Task7.py
@@ -1,15 +1,4 @@
-def insertion_sort(arr, start):
-    for j in range(len(arr)):
-        key = arr[j]
-        i = j - 1
-        while i >= 0 and arr[i] < key:
-            arr[i + 1] = arr[i]
-            i -= 1
-        arr[i + 1] = key
-
-
 def swap(array, ind1, ind2):
-    # print(f"in swap:{array}, {ind1}, {ind2}")
     const = array[ind1]
     array[ind1] = array[ind2]
     array[ind2] = const
@@ -19,16 +8,13 @@ def merge(arr, start1, end1, start2, end2, buff_start):
     lsize = end1 - start1 + 1
     rsize = end2 - start2 + 1
 
-    print(f"l&r: {arr[start1: end1 + 1]}, {arr[start2: end2 + 1]}")
     i = j = k = 0
     while i < lsize and j < rsize:
         if arr[start1 + i] <= arr[start2 + j]:
-            # print(buff_start + k, start1 + i)
             swap(arr, buff_start + k, start1 + i)
             k += 1
             i += 1
         else:
-            # print(buff_start + k, start2 + j)
             swap(arr, buff_start + k, start2 + j)
             k += 1
             j += 1
@@ -43,31 +29,14 @@ def merge(arr, start1, end1, start2, end2, buff_start):
         j += 1
 
 
-def sort_half(array, start, end, buff_start, initial_length):
-    # print(f" halfed array: {array[start: end + 1]}")
-    # print(f"initial length: {initial_length}, start-end: {start - end}")
-    if start < end:
-        middle = start + (end - start) // 2
-
-        sort_half(array, start, middle, buff_start, initial_length)
-        sort_half(array, middle + 1, end, buff_start, initial_length)
-
-        merge(array, start, middle, middle + 1, end, buff_start)
-
-        # print(f"after swapping: {array}")
-        # print(start, end, buff_start)
-
-        # Чтобы нормально сортировалось, я передвигаю числа из буффера обратно в массив,
-        # но в последней итерации этого делать не надо
-        # print(start, end, buff_start)
-        # if start != 0 or end != buff_start - 1:
-
-        print(f"initial length: {initial_length}, start-end: {abs(start - end)}")
-        if abs(end - start + 1) != initial_length:
-            for i in range(end - start + 1):
-                swap(array, start + i, buff_start + i)
-            # array[start + i] = buffer[i]
-        # print(f"array after merge: {array}")
+def insertion_sort(arr, start, end):
+    for j in range(start, end + 1):
+        key = arr[j]
+        i = j - 1
+        while i >= start and arr[i] > key:
+            arr[i + 1] = arr[i]
+            i -= 1
+        arr[i + 1] = key
 
 
 def good_length(start, end):
@@ -77,73 +46,38 @@ def good_length(start, end):
     return middle
 
 
-def merge_sort(array, start, end):
-    # main_middle = good_length(array, start, end)
-    main_middle = start + (end - start) // 2
-    sort_half(array, start, main_middle, main_middle + 1, main_middle - start + 1)  # Сортируем первую половину массива
-    # print(main_middle - start)
+def algorithm_sort(array, start, end):
+    middle = good_length(start, end)  # Decide where to sort (to the right half)
+    merge_buff_sort(array, start, middle, middle + 1)  # Sort left half with right half as a buffer
 
-    # def under_sort(array=array, start=start, end=end):
-    print(f"after first sort: {array}")
-    end = main_middle
-    while end - start > 2:
-        print(f"in while: {start}, {end}")
-
-        middle = good_length(start, end)
-        print(f"middle: {middle}")
-        length = end - middle + 1
-        sort_half(array, middle, main_middle, 0, length)
-
-        print(f"one more sort: {array}")
-
-        print(f"for big merge: {start}, {middle - 1}, {end + 1}, {end * 2 + 1}, buffer: {middle - 1}")
-        merge(array, start, middle - 1, end + 1, end * 2 + 1,
-              middle - 1)  # Мердж не работает, просто ставит рядом отсортированные массивы (пАчИмУ?!?!?!?)
-        print(f"merging: {array}")
-        print(middle)
-        start = 0
-        end = middle
-
-    # under_sort(array, start, end)
-    # if start < end:
-    #     # main_middle = start + (end - start) // 2
-    #     # print(f"middle is {main_middle}")
-    #     # # Сортируем первую половину массива, используя вторую как буфер
-    #     # sort_half(array, start, main_middle, main_middle + 1,
-    #     #           main_middle - start)  # Отсортированные числа стоят на arr[middle:end+1]
-    #
-    #     print(f"after first sort: {arr}")
-    #     middle = start + (main_middle - start) // 2
-    #     # middle = good_length(array, start, main_middle)
-    #     print(f"main_middle: {main_middle}, end: {end}, middle: {middle}")
-    #     print(f"for second sort: {array[middle + 1:main_middle + 1]}")
-    #
-    #     length = main_middle - middle - 1
-    #     # length = abs(start - main_middle) + 1
-    #
-    #     print(f"length is {length}")
-    #     sort_half(array, middle + 1, main_middle, start, length)
-    #
-    #     # print(f"next middle is: {middle}")
-    #     print(array)
-    #     print(f"for merge: {start}, {middle}, {main_middle + 1}, {end}, {middle + 1}")
-    #     merge(array, start, middle - 1, main_middle + 1, end, middle + 1)
-
-    # if start < end:
-    #      middle = start + (end - start) // 2
-    #
-    #     merge_sort(array, start, middle)
-    #     merge_sort(array, middle + 1, end)
-    #
-    #     # buffer = [None] * (end - start + 1)
-    #
-    #     merge(array, start, middle, middle + 1, end)
-    #
-    #     for i in range(len(buffer)):
-    #         array[start + i] = buffer[i]
+    if middle - start < 2:
+        insertion_sort(array, start, end)
+    else:
+        sub_end = middle
+        while sub_end - start > 2:
+            rubbish_middle = good_length(start, sub_end)
+            merge_buff_sort(array, rubbish_middle, sub_end, start)  # Sort right "rubbish half" to the left half
+            merge(array, start, rubbish_middle, sub_end + 1, end, rubbish_middle + 1)
+            sub_end = rubbish_middle
 
 
-arr = [6, 4, 7, 1, 9, 8, 5, 0, 3, 2]
-size = len(arr) - 1
-merge_sort(arr, 0, size)
+def merge_buff_sort(array, start, end, buff):
+    if start < end:
+        middle = start + (end - start) // 2
+        algorithm_sort(array, start, middle)
+        algorithm_sort(array, middle + 1, end)
+        merge(array, start, middle, middle + 1, end, buff)
+
+
+def start(arr):
+    size = len(arr) - 1
+    algorithm_sort(arr, 0, size)
+    # Recursion stops earlier than needed (artificial step) etc. [5, 8, 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8, 9, 9]
+    insertion_sort(arr, 0, size)
+
+
+# arr = [13, 99, 51, 28, 91, 30, 34, 111, 56, 22, 37, 12, 1, 5, 15, 60]
+arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# arr = [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 20]
+start(arr)
 print(arr)
