4a922c313848bdaca61785a81ef3a45ca9600c5c
Taisia Kashintseva <the_smartest_peach@mail.ru>
2023-04-22 21:49:17 +0700

    Task21 done

--- Task21.py
+++ Task21.py
@@ -14,31 +14,9 @@ class Codec:
         :rtype: str
         """
 
-        result = []
-        if root:
-            result.append(root.val)
-            flag = 1
-            Q = [root]
-            while Q:
-                u = Q[0]
-                Q.pop(0)
-                if u.left:
-                    result.append(u.left.val)
-                    Q.append(u.left)
-                else:
-                    result.append(None)
-                if u.right:
-                    result.append(u.right.val)
-                    Q.append(u.right)
-                else:
-                    result.append(None)
-
-            while not result[-1]:  # delete
-                result.pop(-1)
-
-        string = str(result)
-        print(string)
-        return string
+        if not root:
+            return 'x'
+        return str(root.val) + " " + self.serialize(root.left) + " " + self.serialize(root.right)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -46,33 +24,27 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        data = data[1:-1].split(', ')
-        print(data)
-        head = TreeNode()
-        if len(data):
-            Q = [head]
-            head.val = data[0]
-            i = 0
-            while Q:
-                print(Q)
-                u = Q[0]
-                Q.pop(0)
-                # print(u.val)
-                # u.left = TreeNode()
-                # u.right = TreeNode()
-                if 2 * i + 1 < len(data) and u.val != 'None':
-                    print(u.val)
-                    u.left = TreeNode()
-                    u.left.val = data[2 * i + 1] if data[2 * i + 1] != 'null' else None
-                    Q.append(u.left)
-                if 2 * i + 2 < len(data) and u.val != 'None':
-                    print(u.val)
-                    u.right = TreeNode()
-                    u.right.val = data[2 * i + 2] if data[2 * i + 2] != 'null' else None
-                    Q.append(u.right)
-                i += 1
 
-        return head
+        def pre_deser(data):
+            if len(data):
+                head = TreeNode()
+
+                if data[0] != 'x':
+                    head.val = data[0]
+                else:
+                    head = None
+
+                data.pop(0)
+                if head:
+                    head.left = pre_deser(data)
+                    head.right = pre_deser(data)
+                return head
+
+        nodes = []
+        if len(data) != 1:
+            nodes = data.split()
+
+        return pre_deser(nodes)
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
