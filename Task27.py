8cbac823f05116b8daeac2e31581f4b93dccff7a
Taisia Kashintseva <the_smartest_peach@mail.ru>
2023-05-08 20:58:57 +0700

    Task 27

---
+++ Task27.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        result = []
+        counter = {}
+
+        for i in range(len(s) - 9):
+            string = s[i:i + 10]
+            # print(string)
+            if not string in counter:
+                counter[string] = 1
+            else:
+                counter[string] += 1
+                if counter[string] == 2:
+                    result.append(string)
+
+        return result
